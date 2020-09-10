a352849d47212f658e1b61a2e8cc0bfaa236391f
Brent Groves <brent.groves@gmail.com>
2020-09-10 08:58:48 -0400

    Added/Changed Code

---
+++ udp.js
@@ -0,0 +1,26 @@
+var udp = require('dgram');
+
+// -------------------- udp client ----------------
+
+var buffer = require('buffer');
+
+// creating a client socket
+var client = udp.createSocket('udp4');
+
+//                    1234567890
+// var CNC103Frame = `     1, 1`
+//              1234567890
+// CNC103Frame += `         1`
+// var data = Buffer.from(CNC103Frame);
+
+var data=Buffer.from('2514', 'hex');  //DC2
+
+client.send(data,2222,'localhost',function(error){
+//  client.send(data,2221,'localhost',function(error){
+  if(error){
+    client.close();
+  }else{
+    console.log('Data sent !!!');
+  }
+});
+
